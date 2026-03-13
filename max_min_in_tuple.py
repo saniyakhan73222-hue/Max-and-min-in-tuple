c8d69677eea7b9a89a6245b999dded357ff1d36c
saniyakhan73222-hue <saniyakhan73222@bbdu.ac.in>
2026-03-13 11:24:44 +0530

    Update max_min_in_tuple.py

--- max_min_in_tuple.py
+++ max_min_in_tuple.py
@@ -1,1 +1,30 @@
 
+numbers = (12, 45, 7, 23, 56, 89, 34)
+
+
+
+print("Numbers in tuple:", numbers)
+
+
+
+max_num = numbers[0]
+
+min_num = numbers[0]
+
+
+
+for num in numbers:
+
+    if num > max_num:
+
+        max_num = num
+
+    if num < min_num:
+
+        min_num = num
+
+
+
+print("Maximum number:", max_num)
+
+print("Minimum number:", min_num)
